f0a6b97471c1bda879822eafb428abfd23f76190
Cody Perry <perrysdevelopment@gmail.com>
2024-05-27 23:25:49 -0400

    Add canvas area

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,46 @@
 'use client';
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import fabric from "fabric";
 
 import Live from "@/components/Live"
 import Navbar from "@/components/Navbar";
+import { useRef, useEffect } from "react";
+import { initializeFabric, handleCanvasMouseDown } from "@/lib/canvas";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef<boolean>(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>('rectangle');
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef
+      });
+    })
+
+    window.addEventListener('resize', () => {
+      handleResize({ fabricRef })
+    });
+  }, [])
+
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
+
       <section className="flex h-full flex-row">
-        <Live />
+        <LeftSidebar />
+        <Live canvasRef={canvasRef} />
+        <RightSidebar />
       </section>
     </main>
   )
